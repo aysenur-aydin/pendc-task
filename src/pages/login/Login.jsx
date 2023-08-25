20dabe801542fc38374aa00f2bce89803f392eff
Aysenur Aydın <aysenuraydn08@gmail.com>
2023-08-25 15:05:17 +0300

    feat: Upgrade responsive layout for login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,77 +3,77 @@ import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
-    <div className="container min-vw-100 me-lg-0  pe-0 ps-lg-5">
-      <div className="row justify-content-lg-between">
-        <div className="col-xs-12 col-lg-5 py-3 d-flex flex-column justify-content-evenly vh-100">
-          <nav className="navbar p-0">
-            <span className="navbar-brand py-4">
-              <img
-                src={'/logo.svg'}
-                alt="PENDC"
-                className="navbar-brand"
-                width={176}
-              />
-            </span>
-          </nav>
-          <div className="d-flex flex-column ">
-            <div>
-              <h1 className="text-3xl text-gray-800 fw-semibold mb-3">
-                Pendc’ye Hoşgeldiniz
-              </h1>
-              <p className="text-gray-500">
-                Kullanıcı bilgileriniz ile giriş yapabilirsiniz.
-              </p>
-            </div>
-            <div className=" mt-2 w-75">
-              <form className="mt-4">
-                <div className="form-outline mb-3">
-                  <label className="form-label" htmlFor="emailAdress">
-                    E-Posta Adresi
-                  </label>
-                  <input
-                    type="email"
-                    id="emailAdress"
-                    className="form-control"
-                    placeholder="isminiz@mail.com"
-                  />
-                </div>
+    <div className="login-body">
+      <div className="login-container">
+        <div className="login-left">
+          <div className="d-flex flex-column  gap-5">
+            <nav className="navbar pb-5">
+              <span className="navbar-brand py-4">
+                <img
+                  src={'/logo.svg'}
+                  alt="PENDC"
+                  className="navbar-brand"
+                  width={176}
+                />
+              </span>
+            </nav>
+            <div className="d-flex flex-column ">
+              <div>
+                <h1 className="text-3xl text-gray-800 fw-semibold mb-3">
+                  Pendc’ye Hoşgeldiniz
+                </h1>
+                <p className="text-gray-500">
+                  Kullanıcı bilgileriniz ile giriş yapabilirsiniz.
+                </p>
+              </div>
+              <div className=" mt-2 w-75">
+                <form className="mt-4">
+                  <div className="form-outline mb-3">
+                    <label className="form-label" htmlFor="emailAdress">
+                      E-Posta Adresi
+                    </label>
+                    <input
+                      type="email"
+                      id="emailAdress"
+                      className="form-control"
+                      placeholder="isminiz@mail.com"
+                    />
+                  </div>
 
-                <div className="form-outline mb-4">
-                  <label className="form-label" htmlFor="password">
-                    Şifreniz
-                  </label>
-                  <input
-                    type="password"
-                    id="password"
-                    className="form-control"
-                    placeholder="Şifrenizi Giriniz"
-                  />
-                </div>
-                <Link to="/dashboard">
-                  <button
-                    type="button"
-                    className="btn btn-primary btn-lg fw-semibold my-3 w-100"
-                  >
-                    Giriş Yap
-                  </button>
-                </Link>
-                <div className="text-center text-small py-3 mb-3">
-                  <p className="text-gray-500">
-                    Bilgilerinizi hatırlamıyor musunuz?
-                    <a href="#!" className="text-primary fw-semibold">
-                      {' '}
-                      Şifremi Unuttum
-                    </a>
-                  </p>
-                </div>
-              </form>
+                  <div className="form-outline mb-4">
+                    <label className="form-label" htmlFor="password">
+                      Şifreniz
+                    </label>
+                    <input
+                      type="password"
+                      id="password"
+                      className="form-control"
+                      placeholder="Şifrenizi Giriniz"
+                    />
+                  </div>
+                  <Link to="/dashboard">
+                    <button
+                      type="button"
+                      className="btn btn-primary btn-lg fw-semibold my-3 w-100"
+                    >
+                      Giriş Yap
+                    </button>
+                  </Link>
+                  <div className="text-center text-small py-3 mb-3">
+                    <p className="text-gray-500">
+                      Bilgilerinizi hatırlamıyor musunuz?
+                      <a href="#!" className="text-primary fw-semibold">
+                        {' '}
+                        Şifremi Unuttum
+                      </a>
+                    </p>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
-        <div className="col-xs-12 col-lg-6 px-0 min-vh-100">
-          <img src="/company.svg" className="w-100" />
-        </div>
+        <div className="d-xs-none d-md-block login-right"></div>
       </div>
     </div>
   );
